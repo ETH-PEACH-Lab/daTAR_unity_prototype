463c13659c065a3000729d97538614b7d9d6d2b7
lilian <lilian1997.ll@gmail.com>
2024-07-09 09:19:47 +0200

    deleted depraved analyisi mode components

--- Assets/Scripts/ImageTracking.cs
+++ Assets/Scripts/ImageTracking.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-
 public class ImageTracking : MonoBehaviour
 {
     private ARTrackedImageManager trackedImages;
-    public GameObject[] ArPrefabs;
+    public GameObject chartViewCollection;
+    public GameObject outputNode;
+
+    private string nameVisNode = "qr_code_100";
 
     List<GameObject> ARObjects = new List<GameObject>();
 
@@ -36,26 +37,34 @@ public class ImageTracking : MonoBehaviour
         //Create object based on image tracked
         foreach (var trackedImage in eventArgs.added)
         {
-            foreach (var arPrefab in ArPrefabs)
-            {
-                if (trackedImage.referenceImage.name == arPrefab.name)
+            
+                if (trackedImage.referenceImage.name == nameVisNode || trackedImage.referenceImage.name == "marker05")
                 {
-                    var newPrefab = Instantiate(arPrefab, trackedImage.transform.parent);
-                    //Debug.Log(gameObject.transform.position + " ppp " + trackedImage.transform.parent.name);
+                    var newPrefab = Instantiate(outputNode, trackedImage.transform.parent);
+                    newPrefab.name = trackedImage.name;
+                    newPrefab.SetActive(true);
+                    Debug.Log("output node");
+                    ARObjects.Add(newPrefab);
+
+                }
+                else
+                {
+                    var newPrefab = Instantiate(chartViewCollection, trackedImage.transform.parent);
+                //Debug.Log(gameObject.transform.position + " ppp " + trackedImage.transform.parent.name);
 
                     newPrefab.SetActive(true);
                     newPrefab.name = trackedImage.name;
-                    //newPrefab.transform.position = trackedImage.transform.position;
-                    
-                    ARObjects.Add(newPrefab);
-                }
+                //newPrefab.transform.position = trackedImage.transform.position;
+
+                ARObjects.Add(newPrefab);
             }
+            
         }
 
         //Update tracking position
         foreach (var trackedImage in eventArgs.updated)
         {
-            
+
             foreach (var gameObject in ARObjects)
             {
                 if (gameObject.name == trackedImage.name && trackedImage.trackingState == TrackingState.Tracking)
@@ -66,28 +75,15 @@ public class ImageTracking : MonoBehaviour
                     float rotationObject = gameObject.transform.localEulerAngles.y;
                     //Debug.Log(rotationImage + ",rot " + rotationObject);
                     //Debug.Log(Mathf.Asin(rotationImage) + ",rot2 " + Mathf.Asin(rotationObject));
-                    if(Mathf.Abs(rotationImage - rotationObject) > 5)
+                    if (Mathf.Abs(rotationImage - rotationObject) > 10)
                     {
                         //gameObject.transform.rotation = Quaternion.Euler(0, rotationImage, 0);
-                          gameObject.transform.localRotation = trackedImage.transform.localRotation;
+                        gameObject.transform.localRotation = trackedImage.transform.localRotation;
                     }
-                    
-                    //Debug.Log(gameObject.transform.localEulerAngles.y+ "rot3");
-                    
 
-
-
-
-                    // Debug.Log(trackedImage.transform.position + " mmm " + trackedImage.referenceImage.name);
-                    //gameObject.SetActive(trackedImage.trackingState == TrackingState.Tracking);
                 }
             }
         }
 
     }
-
 }
-
-
- 
-
--- Assets/Scripts/ImageTrackingOld.cs
+++ Assets/Scripts/ImageTrackingOld.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-public class TrackingCollectionMode : MonoBehaviour
+
+public class ImageTrackingOld : MonoBehaviour
 {
     private ARTrackedImageManager trackedImages;
-    public GameObject chartViewCollection;
-    public GameObject outputNode;
-
-    private string nameVisNode = "qr_code_100";
+    public GameObject[] ArPrefabs;
 
     List<GameObject> ARObjects = new List<GameObject>();
 
@@ -37,34 +36,26 @@ public class TrackingCollectionMode : MonoBehaviour
         //Create object based on image tracked
         foreach (var trackedImage in eventArgs.added)
         {
-            
-                if (trackedImage.referenceImage.name == nameVisNode || trackedImage.referenceImage.name == "marker05")
+            foreach (var arPrefab in ArPrefabs)
+            {
+                if (trackedImage.referenceImage.name == arPrefab.name)
                 {
-                    var newPrefab = Instantiate(outputNode, trackedImage.transform.parent);
-                    newPrefab.name = trackedImage.name;
+                    var newPrefab = Instantiate(arPrefab, trackedImage.transform.parent);
+                    //Debug.Log(gameObject.transform.position + " ppp " + trackedImage.transform.parent.name);
+
                     newPrefab.SetActive(true);
-                    Debug.Log("output node");
+                    newPrefab.name = trackedImage.name;
+                    //newPrefab.transform.position = trackedImage.transform.position;
+                    
                     ARObjects.Add(newPrefab);
-
                 }
-                else
-                {
-                    var newPrefab = Instantiate(chartViewCollection, trackedImage.transform.parent);
-                //Debug.Log(gameObject.transform.position + " ppp " + trackedImage.transform.parent.name);
-
-                    newPrefab.SetActive(true);
-                    newPrefab.name = trackedImage.name;
-                //newPrefab.transform.position = trackedImage.transform.position;
-
-                ARObjects.Add(newPrefab);
             }
-            
         }
 
         //Update tracking position
         foreach (var trackedImage in eventArgs.updated)
         {
-
+            
             foreach (var gameObject in ARObjects)
             {
                 if (gameObject.name == trackedImage.name && trackedImage.trackingState == TrackingState.Tracking)
@@ -75,15 +66,28 @@ public class TrackingCollectionMode : MonoBehaviour
                     float rotationObject = gameObject.transform.localEulerAngles.y;
                     //Debug.Log(rotationImage + ",rot " + rotationObject);
                     //Debug.Log(Mathf.Asin(rotationImage) + ",rot2 " + Mathf.Asin(rotationObject));
-                    if (Mathf.Abs(rotationImage - rotationObject) > 10)
+                    if(Mathf.Abs(rotationImage - rotationObject) > 5)
                     {
                         //gameObject.transform.rotation = Quaternion.Euler(0, rotationImage, 0);
-                        gameObject.transform.localRotation = trackedImage.transform.localRotation;
+                          gameObject.transform.localRotation = trackedImage.transform.localRotation;
                     }
+                    
+                    //Debug.Log(gameObject.transform.localEulerAngles.y+ "rot3");
+                    
 
+
+
+
+                    // Debug.Log(trackedImage.transform.position + " mmm " + trackedImage.referenceImage.name);
+                    //gameObject.SetActive(trackedImage.trackingState == TrackingState.Tracking);
                 }
             }
         }
 
     }
+
 }
+
+
+ 
+
